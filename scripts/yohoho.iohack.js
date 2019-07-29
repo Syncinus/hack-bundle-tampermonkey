3141cae7165114035b40272848e0f55ff1a2c314
TEAMOVERSONIC <49201819+TEAMOVERSONIC@users.noreply.github.com>
2019-07-29 11:07:33 -0600

    Update yohoho.iohack.js

--- scripts/yohoho.iohack.js
+++ scripts/yohoho.iohack.js
@@ -10,12 +10,10 @@
 
 (function() {
     'use strict';
-var moola = prompt("how much money do you want");
-    localStorage.setItem("coinsOwned",moola);
     localStorage.setItem("playerPet","9");
     localStorage.setItem("unlockedSkins","[35]");
     localStorage.setItem("unlockedPets","[9]");
     localStorage.setItem("playerPetLevel","12");
     localStorage.setItem("playerSkin","35");
-    alert("reload tab");
+    
 })();
